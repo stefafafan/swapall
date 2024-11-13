09196279e6667a59cc332eac12288bb490840fe0
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-13 16:39:44 +0900

    read csv from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(help = "The filepath to the CSV file.", required = true)]
+    csv_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let file = std::fs::File::open(&args.csv_path).expect("Failed to open file");
+    let reader = std::io::BufReader::new(file);
+    let mut csv_reader = csv::Reader::from_reader(reader);
+
+    for result in csv_reader.records() {
+        let record = result.expect("Failed to read record");
+        println!("{:?}", record);
+    }
 }
